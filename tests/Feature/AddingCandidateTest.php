5d93c72927a08e03119fe82ba54d287bb3390721
Rizqy H <rizqy22@gmail.com>
2018-08-07 22:57:45 +0700

    Create base test for adding candidate

---
+++ tests/Feature/AddingCandidateTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddingCandidateTest extends TestCase
+{
+    public function testAddingCandidatePageExists() {}
+
+    public function testFailedAddingCandidate() {}
+
+    public function testSuccessfullyAddingCandidate() {}
+}
