894006406ebfc57817f15f0336a463b960ff5ef5
Rizqy H <rizqy22@gmail.com>
2018-09-02 00:30:41 +0700

    Change candidate model object’s name

--- app/Repositories/EloquentCandidateRepository.php
+++ app/Repositories/EloquentCandidateRepository.php
@@ -4,22 +4,23 @@ namespace App\Repositories;
 
 use App\Candidate;
 use App\Repositories\Contracts\CandidateRepository;
+use Illuminate\Database\Eloquent\Model;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
 class EloquentCandidateRepository implements CandidateRepository
 {
-    private $candidate;
+    private $model;
 
     public function __construct(Candidate $candidate) {
-        $this->candidate = $candidate;
+        $this->model = $candidate;
     }
 
     public function save($candidateData) {
         try {
             $candidateData['id'] = Uuid::uuid4()->toString();
 
-            return $this->candidate->create($candidateData);
+            return $this->model->create($candidateData);
         } catch(UnsatisfiedDependencyException $e) {
             throw $e;
         }
