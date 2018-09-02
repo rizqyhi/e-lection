fe7651cf54a09dfe3103087173a121ae5b3a38e7
Rizqy H <rizqy22@gmail.com>
2018-09-02 17:50:00 +0700

    Remove registration fetaure

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
