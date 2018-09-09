23a4347d4ad9b3e070f98d396e8afee43cdfcca1
Rizqy H <rizqy22@gmail.com>
2018-09-09 11:12:46 +0700

    Update base repository contract to acommodate new requirements

--- app/Repositories/Contracts/BaseRepository.php
+++ app/Repositories/Contracts/BaseRepository.php
@@ -4,5 +4,11 @@ namespace App\Repositories\Contracts;
 
 interface BaseRepository
 {
+    public function getAll();
+
+    public function find($id);
+
     public function save($data);
+
+    public function update($id, $data);
 }
