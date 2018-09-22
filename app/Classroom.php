46e6f305807326a445b38fac81d66db8f48ab7cc
Rizqy H <rizqy22@gmail.com>
2018-09-22 20:10:27 +0700

    Setup relation between classroom and voter

--- app/Classroom.php
+++ app/Classroom.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Classroom extends Model
 {
     protected $fillable = ['name'];
+
+    public function voters()
+    {
+        return $this->hasMany(Voter::class);
+    }
 }
--- app/Voter.php
+++ app/Voter.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Voter extends Model
 {
     protected $fillable = ['classroom_id', 'name', 'access_code'];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
 }
