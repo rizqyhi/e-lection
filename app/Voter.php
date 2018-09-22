37c12bd9dc59d7e287215945f0e73a5d722c15d7
Rizqy H <rizqy22@gmail.com>
2018-09-22 20:42:51 +0700

    Set primary key for voter to not auto incrementing

--- app/Voter.php
+++ app/Voter.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voter extends Model
 {
+    public $incrementing = false;
+
     protected $fillable = ['classroom_id', 'name', 'access_code'];
 
     public function classroom()
