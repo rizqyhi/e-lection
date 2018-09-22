3324b16a8c193362559b97219f4a951bde0cf59b
Rizqy H <rizqy22@gmail.com>
2018-09-22 21:15:21 +0700

    Update voters list page

--- app/Http/Controllers/Dashboard/Voters/ListVoters.php
+++ app/Http/Controllers/Dashboard/Voters/ListVoters.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Dashboard\Voters;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Voter;
 
 class ListVoters extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard.voters.index');
+        $voters = Voter::with('classroom')->get();
+
+        return view('dashboard.voters.index', compact('voters'));
     }
 }
--- resources/views/dashboard/voters/index.blade.php
+++ resources/views/dashboard/voters/index.blade.php
@@ -5,4 +5,36 @@
 @endsection
 
 @section('content')
+    <div class="page-content">
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>NIS</th>
+                        <th>Nama</th>
+                        <th>Kode Akses</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @forelse ($voters as $voter)
+                        <tr>
+                            <td>{{ $voter->id }}</td>
+                            <td>
+                                <strong>{{ $voter->name }}</strong><br>
+                                <em>{{ $voter->classroom->name }}</em>
+                            </td>
+                            <td><code>{{ $voter->access_code }}</code></td>
+                            <td></td>
+                        </tr>
+                    @empty
+                        <tr class="table-info">
+                            <td colspan="5">Belum ada data pemilih</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
