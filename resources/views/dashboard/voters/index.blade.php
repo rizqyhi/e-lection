a7a38cfc8a79654a10987240f6e7709af0f14ace
Rizqy H <rizqy22@gmail.com>
2018-09-23 17:32:19 +0700

    Prepare voters list for datatables implementation

--- resources/views/dashboard/voters/index.blade.php
+++ resources/views/dashboard/voters/index.blade.php
@@ -7,36 +7,25 @@
 @section('content')
     <div class="page-content">
         <div class="table-responsive">
-            <table class="table table-hover">
+            <table class="table table-hover" id="voters-table">
                 <thead>
                     <tr>
                         <th>NIS</th>
                         <th>Nama</th>
+                        <th>Kelas</th>
                         <th>Kode Akses</th>
-                        <th>&nbsp;</th>
+                        {{-- <th>&nbsp;</th> --}}
                     </tr>
                 </thead>
-
-                <tbody>
-                    @forelse ($voters as $voter)
-                        <tr>
-                            <td>{{ $voter->id }}</td>
-                            <td>
-                                <strong>{{ $voter->name }}</strong><br>
-                                <em>{{ $voter->classroom->name }}</em>
-                            </td>
-                            <td><code>{{ $voter->access_code }}</code></td>
-                            <td></td>
-                        </tr>
-                    @empty
-                        <tr class="table-info">
-                            <td colspan="5">Belum ada data pemilih</td>
-                        </tr>
-                    @endforelse
-                </tbody>
             </table>
         </div>
     </div>
 
     @include('dashboard.voters.create')
 @endsection
+
+@push('scripts')
+<script>
+    window.votersDatatablesUrl = "{{ route('dashboard.voters.data') }}";
+</script>
+@endpush
