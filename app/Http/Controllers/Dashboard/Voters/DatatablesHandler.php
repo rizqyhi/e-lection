d95c75fe10c6a38ba91d9633aaccedb312f1c4cc
Rizqy H <rizqy22@gmail.com>
2018-09-29 21:52:43 +0700

    Disable voters action buttons for now
    To be implemented later

--- app/Http/Controllers/Dashboard/Voters/DatatablesHandler.php
+++ app/Http/Controllers/Dashboard/Voters/DatatablesHandler.php
@@ -15,12 +15,13 @@ class DatatablesHandler extends Controller
         return datatables()->of($voters)
             ->editColumn('access_code', '<code>{{$access_code}}</code>')
             ->addColumn('action', function ($voter) {
-                return '
+                /* return '
                 <div class="voter-row-action">
                     <a href="#" class="px-1 mr-2 btn-edit-voter" data-toggle="modal" data-target="#edit-voter-modal" data-voter=\''.$voter->toJson().'\'><i class="ion-md-create"></i></a>
                     <a href="" class="text-danger px-1"><i class="ion-md-trash"></i></a>
                 </div>
-                ';
+                '; */
+                return '';
             })
             ->rawColumns(['access_code', 'action'])
             ->toJson();
