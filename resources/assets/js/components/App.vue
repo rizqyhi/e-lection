a62272c0f91e10e19e8d9edcab939c438bdb9fb6
Rizqy H <rizqy22@gmail.com>
2018-09-30 11:07:30 +0700

    Use Vue for voting page

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <stage-login></stage-login>
+    </div>
+</template>
+
+<script>
+import StageLogin from './StageLogin'
+
+export default {
+    components: { StageLogin }
+}
+</script>
+
+<style>
+    html, body, #app {
+        height: 100%;
+    }
+</style>
---
+++ resources/assets/js/components/StageLogin.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container-fluid" style="height: 100%">
+        <div class="row d-flex align-items-center" style="height: 100%">
+            <div class="col-md-4 d-flex align-items-center justify-content-center login-sidebar">
+                <h2>E-Lection</h2>
+            </div>
+            <div class="col-md-8 d-flex justify-content-center">
+                <form class="login-form">
+                    <div class="alert alert-info">Silakan login dengan menggunakan NIS dan kode akses yang telah diberikan.</div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="NIS">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control text-monospace" placeholder="*****">
+                    </div>
+                    <button class="btn btn-primary btn-block">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.login-sidebar {
+    height: 100%;
+    background: #8E2DE2;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(60deg, #4A00E0, #8E2DE2);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(60deg, #4A00E0, #8E2DE2); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    color: #fff;
+}
+.login-form {
+    width: 50%;
+}
+</style>
