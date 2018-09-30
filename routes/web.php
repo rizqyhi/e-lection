3385c4251f6b7a98042a2d8bd01d5662f054e052
Rizqy H <rizqy22@gmail.com>
2018-09-30 11:05:55 +0700

    Create base views for voting page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', VoteController::class);
 
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
