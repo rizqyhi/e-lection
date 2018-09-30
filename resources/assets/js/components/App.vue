3ee2c689459c10c3f688359601877c8865ad37f3
Rizqy H <rizqy22@gmail.com>
2018-09-30 23:54:25 +0700

    Move vote page sidebar to App root component

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,6 +1,15 @@
 <template>
     <div id="app">
-        <stage-login></stage-login>
+        <div class="container-fluid" style="height: 100%">
+            <div class="row d-flex align-items-center" style="height: 100%">
+                <div class="col-md-4 d-flex align-items-center justify-content-center sidebar">
+                    <h2>E-Lection</h2>
+                </div>
+                <div class="col-md-8 d-flex justify-content-center">
+                    <stage-login @login="goToVoteStage"></stage-login>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,11 +18,25 @@ import StageLogin from './StageLogin'
 
 export default {
     components: { StageLogin }
+
+    methods: {
+        goToVoteStage (voter) {
+            console.log(voter)
+        }
+    }
 }
 </script>
 
 <style>
-    html, body, #app {
-        height: 100%;
-    }
+html, body, #app {
+    height: 100%;
+}
+
+.sidebar {
+    height: 100%;
+    background: #8E2DE2;
+    background: -webkit-linear-gradient(60deg, #4A00E0, #8E2DE2);
+    background: linear-gradient(60deg, #4A00E0, #8E2DE2);
+    color: #fff;
+}
 </style>
--- resources/assets/js/components/StageLogin.vue
+++ resources/assets/js/components/StageLogin.vue
@@ -1,27 +1,18 @@
 <template>
-    <div class="container-fluid" style="height: 100%">
-        <div class="row d-flex align-items-center" style="height: 100%">
-            <div class="col-md-4 d-flex align-items-center justify-content-center login-sidebar">
-                <h2>E-Lection</h2>
-            </div>
-            <div class="col-md-8 d-flex justify-content-center">
-                <form class="login-form" @submit.prevent="submitLoginForm()">
-                    <div class="alert alert-info">Silakan login dengan menggunakan NIS dan kode akses yang telah diberikan.</div>
-                    <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="NIS" v-model="voter.id">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control text-monospace" placeholder="*****" v-model="voter.access_code">
-                    </div>
-                    <button type="submit" class="btn btn-brand btn-block" :disabled="disableSubmitButton">
-                        <loading-icon v-if="isLoading"></loading-icon>
-                        <span v-else>Login</span>
-                    </button>
-                </form>
-            </div>
+    <form class="login-form" @submit.prevent="submitLoginForm()">
+        <div class="alert alert-info">Silakan login dengan menggunakan NIS dan kode akses yang telah diberikan.</div>
+        <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="NIS" v-model="voter.id">
         </div>
-    </div>
+        <div class="form-group">
+            <input type="text" class="form-control text-monospace" placeholder="*****" v-model="voter.access_code">
+        </div>
+        <button type="submit" class="btn btn-brand btn-block" :disabled="disableSubmitButton">
+            <loading-icon v-if="isLoading"></loading-icon>
+            <span v-else>Login</span>
+        </button>
+    </form>
 </template>
 
 <script>
@@ -49,8 +40,10 @@ export default {
 
             axios.post('/api/voters/auth', this.voter)
                 .then(response => {
-                    window.localStorage.setItem('voter', JSON.stringify(response.data))
+                    let voter = response.data
+                    window.localStorage.setItem('voter', JSON.stringify(voter))
                     this.isLoading = false
+                    this.$emit('login', voter)
                 })
                 .catch(error => {
                     this.errorMessage = error.response.data.message
@@ -62,13 +55,6 @@ export default {
 </script>
 
 <style>
-.login-sidebar {
-    height: 100%;
-    background: #8E2DE2;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(60deg, #4A00E0, #8E2DE2);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(60deg, #4A00E0, #8E2DE2); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    color: #fff;
-}
 .login-form {
     width: 50%;
 }
