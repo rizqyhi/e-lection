dd9a2043d9e9584e1ab1fc8036e06279525e95ea
Rizqy H <rizqy22@gmail.com>
2018-10-01 15:34:40 +0700

    Update to provide basic dashboard report

--- app/Http/Controllers/Dashboard/Home.php
+++ app/Http/Controllers/Dashboard/Home.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Candidate;
+use App\Classroom;
+use App\Voter;
 
 class Home extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard.home');
+        $stats = [
+            'candidates' => Candidate::count(),
+            'classrooms' => Classroom::count(),
+            'voters' => Voter::count()
+        ];
+
+        $voters = [
+            'voted' => Voter::has('vote')->count(),
+            'unvoted' => Voter::doesntHave('vote')->count()
+        ];
+
+        $candidates = Candidate::withCount('votes')->get();
+
+        return view('dashboard.home', compact('stats', 'voters', 'candidates'));
     }
 }
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,23 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
+
+@section('sidebar')
+<div class="card">
+    <div class="card-header">Statistik</div>
+
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <span>Kandidat</span>
+                <strong>{{ $stats['candidates'] }}</strong>
+            </div>
+        </li>
+        <li class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <span>Kelas</span>
+                <strong>{{ $stats['classrooms'] }}</strong>
+            </div>
+        </li>
+        <li class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <span>Pemilih</span>
+                <strong>{{ $stats['voters'] }}</strong>
+            </div>
+        </li>
+    </ul>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="card">
+    <div class="card-header">Perolehan Suara</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <ul class="list-group list-group-flush">
+        @foreach ($candidates as $candidate)
+            <li class="list-group-item">
+                <div class="d-flex w-100 align-items-center">
+                    <span class="badge badge-primary badge-pill">{{ $candidate->no }}</span>
+                    <img src="{{ $candidate->photo_url }}" alt="" width="48" height="48" class="rounded-circle ml-3">
+                    <span class="ml-3">{{ $candidate->name }}</span>
+                    <strong class="ml-auto">{{ $candidate->votes_count }}</strong>
+                    <span class="mx-2">/</span>
+                    <strong>{{ number_format($candidate->votes_count / $stats['voters'] * 100, 2, ',', '.') }}%</strong>
                 </div>
-            </div>
-        </div>
-    </div>
+            </li>
+        @endforeach
+    </ul>
 </div>
 @endsection
